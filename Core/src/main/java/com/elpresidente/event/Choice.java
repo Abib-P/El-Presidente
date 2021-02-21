65c07427b4396d85acb64457de2bd0f7c5874f36
paula <paulabib17@gmail.com>
2021-02-21 18:50:10 +0100

    add choice

--- Core/src/main/java/com/elpresidente/event/Choice.java
+++ Core/src/main/java/com/elpresidente/event/Choice.java
@@ -1,8 +1,18 @@
 package com.elpresidente.event;
 
+import java.util.Map;
+
 public class Choice {
     String name;
     Event relatedEvent;
+    Map<String,Integer> actionOnFaction;
+    Map<String,Integer> actionOnFactor;
+    Integer partisanGained;
 
-
+    public Choice(String name, Map<String,Integer> actionOnFaction, Map<String,Integer> actionOnFactor, Integer partisanGained){
+        this.name = name;
+        this.actionOnFaction = actionOnFaction;
+        this.actionOnFactor = actionOnFactor;
+        this.partisanGained = partisanGained;
+    }
 }
