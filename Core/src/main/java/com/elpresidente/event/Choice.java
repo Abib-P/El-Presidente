70b44e0b99e955a63c222dc507d4a1b5c5296d06
paula <paulabib17@gmail.com>
2021-02-21 18:12:53 +0100

    add GameParameter

--- Core/src/main/java/com/elpresidente/event/Choice.java
+++ Core/src/main/java/com/elpresidente/event/Choice.java
@@ -2,4 +2,7 @@ package com.elpresidente.event;
 
 public class Choice {
     String name;
+    Event relatedEvent;
+
+
 }
---
+++ Core/src/main/java/com/elpresidente/game/GameParameter.java
@@ -0,0 +1,4 @@
+package com.elpresidente.game;
+
+public class GameParameter {
+}
