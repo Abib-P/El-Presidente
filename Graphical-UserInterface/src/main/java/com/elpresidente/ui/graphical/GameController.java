4cd4075696f89ae7215c00276534595c4e4cccf2
Clement <clementhottotpro@gmail.com>
2021-03-04 23:25:31 +0100

    refactor the displayGameInfo ui fixed all warning in GraphicalUserInterface.java

--- Graphical-UserInterface/src/main/java/com/elpresidente/ui/graphical/GameController.java
+++ Graphical-UserInterface/src/main/java/com/elpresidente/ui/graphical/GameController.java
@@ -1,5 +1,8 @@
 package com.elpresidente.ui.graphical;
 
+import com.elpresidente.faction.Faction;
+import com.elpresidente.factions.Factions;
+import com.elpresidente.game.Game;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
@@ -13,7 +16,7 @@ import javafx.scene.layout.VBox;
 
 public class GameController {
 
-    public PieChart pie;
+    public PieChart pieChart;
     public BarChart<String, Integer> barChart;
     public VBox vBox;
 
@@ -26,8 +29,8 @@ public class GameController {
 
     @FXML
     public void initialize() {
-        pie.getData().add(new PieChart.Data("Industry", 0));
-        pie.getData().add(new PieChart.Data("Agriculture", 0));
+        pieChart.getData().add(new PieChart.Data("Industry", 0));
+        pieChart.getData().add(new PieChart.Data("Agriculture", 0));
 
         XYChart.Series<Integer, String> series = new XYChart.Series<>();
         series.setName("");
@@ -39,9 +42,59 @@ public class GameController {
         series.getData().add(new XYChart.Data<>(0, ""));
         treasuryChart.getData().add( series);
 
+        XYChart.Series<String, Integer> satisfactionSeries, partisanSeries;
+
+        ObservableList< XYChart.Series<String, Integer>> data = barChart.getData();
+        satisfactionSeries = new XYChart.Series<>();
+        satisfactionSeries.setName("Satisfaction");
+
+        partisanSeries = new XYChart.Series<>();
+        partisanSeries.setName("Partisan number");
+
+        data.add( satisfactionSeries);
+        data.add( partisanSeries);
+
     }
 
-    public void addEven(Pane pane){
+    public void updateGameInfo(Game game){
+        pieChart.getData().get(0).setPieValue( game.getIndustries());
+        pieChart.getData().get(1).setPieValue( game.getAgriculture());
+
+        XYChart.Data< Integer, String> data = foodChart.getData().get(0).getData().get(0);
+        data.setXValue( game.getFood());
+        data = treasuryChart.getData().get(0).getData().get(0);
+        data.setXValue( game.getTreasury());
+
+        displayFactionsInfo(game.getFactionManager() );
+    }
+
+    private void displayFactionsInfo(Factions factions){
+
+        XYChart.Series<String, Integer> satisfactionSeries, partisanSeries;
+
+        satisfactionSeries = barChart.getData().get(0);
+        partisanSeries = barChart.getData().get(1);
+
+        if( barChart.getData().get(0).getData().isEmpty() ){
+            for (Faction faction : factions.getFactions()) {
+                satisfactionSeries.getData().add(new XYChart.Data<>(faction.getName(), faction.getSatisfaction()) );
+                partisanSeries.getData().add(new XYChart.Data<>(faction.getName(), faction.getPartisanNumber()) );
+            }
+        }
+
+        for (XYChart.Data<String, Integer> data : satisfactionSeries.getData() ) {
+            data.setYValue( factions.getFactionByName(data.getXValue()).getSatisfaction() );
+        }
+        for (XYChart.Data<String, Integer> data : partisanSeries.getData() ) {
+            data.setYValue( factions.getFactionByName(data.getXValue()).getPartisanNumber() );
+        }
+
+        for (Faction faction: factions.getFactions()) {
+            System.out.println(faction.getName()+": "+faction.getSatisfaction()+"%");
+        }
+    }
+
+    public void setEvent(Pane pane){
         if(hBox.getChildren().size() == 0){
             hBox.getChildren().add(pane);
         }else{
--- Graphical-UserInterface/src/main/java/com/elpresidente/ui/graphical/GraphicalUserInterface.java
+++ Graphical-UserInterface/src/main/java/com/elpresidente/ui/graphical/GraphicalUserInterface.java
@@ -8,28 +8,13 @@ import com.elpresidente.game.Saisons;
 import com.elpresidente.ui.UserInterface;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
-import javafx.concurrent.Task;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.PieChart;
-import javafx.scene.chart.XYChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.Dialog;
-import javafx.scene.control.DialogPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 /**
  * JavaFX GraphicalUserInterface
@@ -38,11 +23,10 @@ public class GraphicalUserInterface extends Application implements UserInterface
 
     public static GraphicalUserInterface ui = null;
 
-    private static Scene scene;
     private Stage stage;
 
     private Scene gameControllerPane;
-    public GameController gameController = null;
+    private final GameController gameController;
 
     private Scene scenarioSelectionScene;
     private final ScenarioSelectionController scenarioSelectionController;
@@ -102,21 +86,9 @@ public class GraphicalUserInterface extends Application implements UserInterface
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
        this.stage = stage;
        this.stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        Parent parent;
-        FXMLLoader fxmlLoader = new FXMLLoader(GraphicalUserInterface.class.getResource(fxml + ".fxml"));
-        parent =  fxmlLoader.load();
-
-        return parent;
     }
 
     public static void main(String[] args) {
@@ -126,66 +98,11 @@ public class GraphicalUserInterface extends Application implements UserInterface
     @Override
     public void displayGameInfo(Game game) {
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                PieChart chart = gameController.pie;
-                chart.getData().get(0).setPieValue( game.getIndustries());
-                chart.getData().get(1).setPieValue( game.getAgriculture());
-
-                XYChart.Data< Integer, String> data = gameController.foodChart.getData().get(0).getData().get(0);
-                data.setXValue( game.getFood());
-                data = gameController.treasuryChart.getData().get(0).getData().get(0);
-                data.setXValue( game.getTreasury());
-
-            }
-        });
+        Platform.runLater(() -> gameController.updateGameInfo(game));
 
         System.out.println("money: "+game.getTreasury()+"\nfood: "+ game.getFood()+"\nIndustry: "+game.getIndustries()+"% Agriculture: "+game.getAgriculture()+"%");
-        displayFactionsInfo(game.getFactionManager());
     }
-    private void displayFactionsInfo(Factions factions){
-        BarChart<String, Integer> chart = gameController.barChart;
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                XYChart.Series<String, Integer> satisfactionSeries, partisanSeries;
-
-                if( chart.getData().isEmpty() ){
-                    ObservableList< XYChart.Series<String, Integer>> data = chart.getData();
-                    satisfactionSeries = new XYChart.Series<String, Integer>();
-                    satisfactionSeries.setName("Satisfaction");
-
-                    partisanSeries = new XYChart.Series<String, Integer>();
-                    partisanSeries.setName("Partisan number");
-
-                    data.add( satisfactionSeries);
-                    data.add( partisanSeries);
-
-                    for (Faction faction : factions.getFactions()) {
-                        satisfactionSeries.getData().add( new XYChart.Data<String, Integer>(faction.getName(), faction.getSatisfaction()) );
-                        partisanSeries.getData().add( new XYChart.Data<String, Integer>(faction.getName(), faction.getPartisanNumber()) );
-                    }
-                }else{
-                    satisfactionSeries = chart.getData().get(0);
-                    partisanSeries = chart.getData().get(1);
-                }
-
-                for (XYChart.Data<String, Integer> data : satisfactionSeries.getData() ) {
-                    data.setYValue( factions.getFactionByName(data.getXValue()).getSatisfaction() );
-                }
-                for (XYChart.Data<String, Integer> data : partisanSeries.getData() ) {
-                    data.setYValue( factions.getFactionByName(data.getXValue()).getPartisanNumber() );
-                }
-
-            }
-        });
-
-        for (Faction faction: factions.getFactions()) {
-            System.out.println(faction.getName()+": "+faction.getSatisfaction()+"%");
-        }
-    }
 
     @Override
     public void displayTreasury(int treasury) {
@@ -215,23 +132,17 @@ public class GraphicalUserInterface extends Application implements UserInterface
     @Override
     public String selectScenario(Map<String, String> AllScenarioNames) {
         String scenario;
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                scenarioSelectionController.setData( AllScenarioNames);
-                stage.setScene( scenarioSelectionScene);
-                stage.sizeToScene();
+        Platform.runLater(() -> {
+            scenarioSelectionController.setData( AllScenarioNames);
+            stage.setScene( scenarioSelectionScene);
+            stage.sizeToScene();
 
-            }
         });
 
         scenario = AllScenarioNames.get( scenarioSelectionController.getScenario());
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                stage.setScene( gameControllerPane);
-                stage.sizeToScene();
-            }
+        Platform.runLater(() -> {
+            stage.setScene( gameControllerPane);
+            stage.sizeToScene();
         });
 
         return scenario;
@@ -249,25 +160,17 @@ public class GraphicalUserInterface extends Application implements UserInterface
 
     @Override
     public void displaySeason(Saisons season) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                gameController.seasonLabel.setText( season.toString() );
-            }
-        });
+        Platform.runLater(() -> gameController.seasonLabel.setText( season.toString() ));
     }
 
     @Override
     public Choice getChoice(Event event) {
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                gameController.actionLabel.setText( "New Event" );
-                gameController.addEven( eventSelectorPane);
-                eventChoiceSelector.setEventChoice(event);
-                stage.sizeToScene();
-            }
+        Platform.runLater(() -> {
+            gameController.actionLabel.setText( "New Event" );
+            gameController.setEvent( eventSelectorPane);
+            eventChoiceSelector.setEventChoice(event);
+            stage.sizeToScene();
         });
 
         return eventChoiceSelector.getEventChoice(event);
@@ -281,16 +184,13 @@ public class GraphicalUserInterface extends Application implements UserInterface
     @Override
     public Faction selectFactionToCorrupt(Factions factions, int treasury) {
 
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
+        Platform.runLater(() -> {
 
-                gameController.actionLabel.setText( "Corruption Time" );
+            gameController.actionLabel.setText( "Corruption Time" );
 
-                factionCorruptionController.setData(factions, treasury);
+            factionCorruptionController.setData(factions, treasury);
 
-                gameController.addEven( factionCorruptionPane);
-            }
+            gameController.setEvent( factionCorruptionPane);
         });
 
         return  factionCorruptionController.getFaction();
@@ -298,13 +198,10 @@ public class GraphicalUserInterface extends Application implements UserInterface
 
     @Override
     public int getMarketAmount(int food, int necessaryFood,int treasury) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                gameController.actionLabel.setText( "Food Market" );
-                marketController.setData(food, necessaryFood, treasury);
-                gameController.addEven(marketPane);
-            }
+        Platform.runLater(() -> {
+            gameController.actionLabel.setText( "Food Market" );
+            marketController.setData(food, necessaryFood, treasury);
+            gameController.setEvent(marketPane);
         });
 
         return marketController.getAmount();
