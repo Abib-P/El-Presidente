ba42501f6ef9a87332b3160882aeace5500523bd
nathanaelto <nathanael.to@free.fr>
2021-03-07 18:25:33 +0100

    test save

--- Core/src/main/java/com/elpresidente/save/Save.java
+++ Core/src/main/java/com/elpresidente/save/Save.java
@@ -6,6 +6,8 @@ import com.google.gson.GsonBuilder;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
 
 public class Save {
 
@@ -13,12 +15,15 @@ public class Save {
         final GsonBuilder builder = new GsonBuilder();
         final Gson gson = builder.create();
 
-        gson.toJson(game.getGameStatement(pseudo), new FileWriter("saves\\" + pseudo + ".json"));
+//        gson.toJson(game.getGameStatement(pseudo), new FileWriter("saves\\" + pseudo + ".json"));
 
         final String json = new Gson().toJson(game.getGameStatement(pseudo));
         System.out.println("Resultat save for " + pseudo + " = " + json);
 
+        Writer writer = new FileWriter("saves\\" + pseudo + ".json", StandardCharsets.UTF_8);
+        new Gson().toJson(game.getGameStatement(pseudo), writer);
+
+        writer.close();
+
     }
-
-
 }
