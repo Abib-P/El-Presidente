32ad2c275c9ddba7866e26f459efc1b597bea12c
nathanaelto <nathanael.to@free.fr>
2021-03-07 18:26:19 +0100

    add LoadRepositery

---
+++ Json-Repository/src/main/java/com/elpresidente/repository/json/LoadRepositery.java
@@ -0,0 +1,35 @@
+package com.elpresidente.repository.json;
+
+import com.elpresidente.event.Event;
+import com.elpresidente.faction.Faction;
+import com.elpresidente.game.GameParameter;
+import com.elpresidente.repository.Repository;
+
+import java.util.List;
+
+public class LoadRepositery implements Repository {
+    @Override
+    public List<Faction> getAllFactions() {
+        return null;
+    }
+
+    @Override
+    public List<Event> getAllEvent() {
+        return null;
+    }
+
+    @Override
+    public GameParameter getAllGameParameter() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getStory() {
+        return null;
+    }
+}
